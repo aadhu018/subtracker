98c910014b263fa5f70d98ec5d608e8b7c8d513f
aadhu018 <aadhu2001@gmail.com>
2026-01-19 18:37:23 -0500

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = "force-dynamic";
+
 import SubscriptionSummary from "@/components/SubscriptionSummary";
 import Login from "@/components/Login";
 import SubscriptionsDisplay from "@/components/SubscriptionsDisplay";
